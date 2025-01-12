b9f35104368f7e167681d06aa8d6ab3cd476a191
pouriasabaghi <pouriasabaghi@gmail.com>
2025-01-12 22:01:45 +0330

    🐛 fix: observer for change and make dir auto to prevent conflict rtl style

--- content.js
+++ content.js
@@ -1,3 +1,23 @@
+function setAutoDirection() {
+  const elements = document.querySelectorAll(".ds-markdown");
+  elements.forEach((element) => {
+    if (!element.hasAttribute("dir")) {
+      element.setAttribute("dir", "auto");
+    }
+  });
+}
+
+function stopObserverAndReset(observer) {
+  if (observer) {
+    observer.disconnect();
+  }
+
+  const elements = document.querySelectorAll('[dir="auto"]');
+  elements.forEach((element) => {
+    element.removeAttribute("dir");
+  });
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   document.head.insertAdjacentHTML(
     "beforeend",
@@ -7,11 +27,25 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
     </stye>`
   );
+
+  let observer;
+
   if (request.action === "rtl") {
-    document.body.style.direction = "rtl";
-    document.body.setAttribute(dir, "rtl");
+    observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.addedNodes.length) {
+          setAutoDirection();
+        }
+      });
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+
+    setAutoDirection();
   } else {
-    document.body.style.direction = "ltr";
-    document.body.setAttribute(dir, "ltr");
+    stopObserverAndReset(observer);
   }
 });
--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   const checkbox = document.querySelector(".switch__checkbox");
-  const trigger = document.querySelector(".switch__trigger");
+  const switchLabel = document.querySelector(".switch");
+
+  const lastDirection = localStorage.getItem("direction") || "ltr" ;
+  
+  if(lastDirection === "rtl"){
+
+    checkbox.checked = true;
+    switchLabel.classList.add("active");
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "rtl",
+      });
+    });
+  }
 
   checkbox.addEventListener("change", handleDirection);
-
   function handleDirection(e) {
     e.target.closest(".switch").classList.toggle("active");
 
+    const direction = !checkbox.checked ? "ltr" : "rtl";
+
+    localStorage.setItem("direction", direction);
+
     const request = {
-      action: !checkbox.checked ? "ltr" : "rtl",
+      action: direction,
     };
 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
